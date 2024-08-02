fbf6a8518ffee281fc8969e373482f1b441cfc9a
rushiraj-vala <rushirajvala94@gmail.com>
2024-08-02 12:14:34 +0530

    update: Removed Tkinter imports from OCR.py

--- backapp/OCR.py
+++ backapp/OCR.py
@@ -3,8 +3,8 @@ The Class for Optical Character Recognition
 """
 import pytesseract as pyt
 import os
-from PIL import ImageTk, Image
-from datetime import datetime
+# from PIL import ImageTk, Image
+# from datetime import datetime
 import io
 
 
@@ -23,17 +23,17 @@ class OCRManager():
 
         return pyt.image_to_data('temp_image.png', output_type='data.frame')
 
-    def imgTkTopng(self, TkImage):
-        current_date = datetime.now()
-        string_datetime = current_date.strftime("%Y%m%d_%H%M%S")+".png"
-        new_file_path = os.path.join(os.getcwd(), string_datetime)
-        print(new_file_path)
-        imagePIL = ImageTk.getimage(TkImage)
-        imagePIL.save(new_file_path, "PNG")
+    # def imgTkTopng(self, TkImage):
+    #     current_date = datetime.now()
+    #     string_datetime = current_date.strftime("%Y%m%d_%H%M%S")+".png"
+    #     new_file_path = os.path.join(os.getcwd(), string_datetime)
+    #     print(new_file_path)
+    #     imagePIL = ImageTk.getimage(TkImage)
+    #     imagePIL.save(new_file_path, "PNG")
 
-        img_byte_arr = io.BytesIO()
-        imagePIL.save(img_byte_arr, format='PNG')
-        imageByte = img_byte_arr.getvalue()
+    #     img_byte_arr = io.BytesIO()
+    #     imagePIL.save(img_byte_arr, format='PNG')
+    #     imageByte = img_byte_arr.getvalue()
 
-        imagePIL = Image.open(new_file_path)
-        return imagePIL, imageByte, string_datetime
+    #     imagePIL = Image.open(new_file_path)
+    #     return imagePIL, imageByte, string_datetime
